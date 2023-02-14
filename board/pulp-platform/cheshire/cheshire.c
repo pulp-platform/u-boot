c38a5626d67dc446b38920f48d920549da478c6f
Christopher Reinwardt <creinwar@ethz.ch>
2023-02-14 01:23:58 +0100

    Update device tree and config. Configure LLC entirely to cache after relocation to high memory

--- board/pulp-platform/cheshire/cheshire.c
+++ board/pulp-platform/cheshire/cheshire.c
@@ -1,6 +1,19 @@
 #include <init.h>
 
+#define CHESHIRE_AXI_LLC_ADDR 0x02001000
+
 int board_init(void) {
+  volatile unsigned int *axi_llc = (volatile unsigned int *) CHESHIRE_AXI_LLC_ADDR;
+  // Configure the LLC fully as cache
+  // CFG_SPM_LOW = 0x0
+  axi_llc[0] = 0x0;
+
+  // CFG_SPM_HIGH = 0x0
+  axi_llc[1] = 0x0;
+
+  // COMMIT_CFG = 0x1
+  axi_llc[4] = 0x1;
+
   return 0;
 }
 
